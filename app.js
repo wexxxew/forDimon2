7d1eeb4743ce5302955d2e3da48b9482e8c343ce
wexxxew <143607849+wexxxew@users.noreply.github.com>
2023-11-19 11:16:39 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragover)
+    placeholder.addEventListener('drop', dragdrop)
+}
+
+function dragstart(event){
+    event.target.classList.add('hold')
+    setTimeout(() => event.target.classList.add ('hide'), 0)
+}
+
+function dragend(event){
+    event.target.className = 'item'
+}
+
+function dragover(event){
+    console.log('drag over')
+}
+
+function dragenter(event){
+    event.target.classList.add('hovered')
+    console.log('drag enter')
+}
+
+function dragleave(event){
+    event.target.classList.remove('hovered')
+    console.log('drag leave')
+}
+
+function dragdrop(event){
+    console.log('drag drop')
+}
